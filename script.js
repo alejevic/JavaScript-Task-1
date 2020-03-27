06cb05dd3f4fb058ca160d0f426eb940ef1a5154
alejevic <61966180+alejevic@users.noreply.github.com>
2020-03-27 02:11:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+let name = "Victor Edward Aleje"; let course = "Frontend: All Courses, Mobile: All Courses, Design: All Courses" console.log("My name is: " +- name); console.log("Courses Offered are - " +- course)
+const data = [];
+for (let i = 0; i <= 200; i+-+-) {
+if (i % 2 === 0) {
+data.push(i);
+}
+}
+console.log(data);+AKA-
